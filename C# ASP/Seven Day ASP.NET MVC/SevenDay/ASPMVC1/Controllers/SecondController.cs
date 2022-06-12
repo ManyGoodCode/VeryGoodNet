92fe3174bbb75c1c3b14d5160763f274fa7940a2
Wu <1294039604@qq.com>
2022-06-12 21:45:22 +0800

    Model 与 ViewModel的关系

--- C# ASP/Seven Day ASP.NET MVC/SevenDay/ASPMVC1/Controllers/SecondController.cs
+++ C# ASP/Seven Day ASP.NET MVC/SevenDay/ASPMVC1/Controllers/SecondController.cs
@@ -9,6 +9,7 @@ namespace ASPMVC1.Controllers
 {
     public class SecondController : System.Web.Mvc.Controller
     {
+        // ViewData传值
         // 1. 视图一定要在 Views ->  Second 文件夹中，不同控制器的命名可以相同。返回的名称要写在 return View(viewName: "View1");
         // 2. 浏览器按F12可以查看具体显示情况
         public ActionResult GetViewData()
@@ -24,7 +25,7 @@ namespace ASPMVC1.Controllers
             return View(viewName: "View1");
         }
 
-
+        // ViewBag传值
         public ActionResult GetViewBag()
         {
             Employee e = new Employee()
@@ -36,6 +37,22 @@ namespace ASPMVC1.Controllers
 
             ViewBag.Employee = e;
             return View(viewName: "View2");
+        }
+
+        // 强类型视图 Model 与 ViewModel的关系
+        // ！！！！！！！！！！！！！  Model和ViewModel 是互相独立的，Controller将根据Model对象创建并初始化ViewModel对象。
+        // http://localhost:62939/Second/StrongView?salary=1030 渲染成绿色
+        // http://localhost:62939/Second/StrongView?salary=2000 渲染成黄色
+        public ActionResult StrongView(int salary)
+        {
+            Employee e = new Employee()
+            {
+                FirstName = "Li",
+                LastName = " Xiao",
+            };
+
+            e.Salary = salary;
+            return View("View3", e);
         }
     }
 }
