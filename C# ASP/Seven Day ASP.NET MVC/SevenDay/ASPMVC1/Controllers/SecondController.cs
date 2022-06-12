970b4d8121d261bff39929c6e4cb9d0411cdc815
Wu <1294039604@qq.com>
2022-06-12 21:16:51 +0800

    视图显示 ViewData / ViewBag

--- C# ASP/Seven Day ASP.NET MVC/SevenDay/ASPMVC1/Controllers/SecondController.cs
+++ C# ASP/Seven Day ASP.NET MVC/SevenDay/ASPMVC1/Controllers/SecondController.cs
@@ -11,7 +11,7 @@ namespace ASPMVC1.Controllers
     {
         // 1. 视图一定要在 Views ->  Second 文件夹中，不同控制器的命名可以相同。返回的名称要写在 return View(viewName: "View1");
         // 2. 浏览器按F12可以查看具体显示情况
-        public ActionResult GetEmployee()
+        public ActionResult GetViewData()
         {
             Employee e = new Employee()
             {
@@ -22,6 +22,20 @@ namespace ASPMVC1.Controllers
 
             ViewData["Employee"] = e;
             return View(viewName: "View1");
+        }
+
+
+        public ActionResult GetViewBag()
+        {
+            Employee e = new Employee()
+            {
+                FirstName = "Li",
+                LastName = " Xiao",
+                Salary = 1234
+            };
+
+            ViewBag.Employee = e;
+            return View(viewName: "View2");
         }
     }
 }
