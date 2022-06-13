9efc278bac133db32ba438a669ab55d2f5cc3fd7
Wu <1294039604@qq.com>
2022-06-13 22:49:38 +0800

    添加模型

---
+++ C# ASP/Seven Day ASP.NET MVC/SevenDay/ASPMVC1/Models/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASPMVC1.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Salary { get; set; }
+    }
+
+    public class PersonModel
+    {
+        public string Name { get; set; }
+
+        public int Salary { get; set; }
+        public string Color { get; set; }
+    }
+
+    public class PersonLstView
+    {
+        public List<PersonModel> PersonModels { get; set; }
+
+        public string LoginUser { get; set; }
+    }
+}
---
+++ C# ASP/Seven Day ASP.NET MVC/SevenDay/ASPMVC1/Models/School.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPMVC1.Models
+{
+    public class School
+    {
+        public string Name { get; set; }
+
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public string Province { get; set; }
+        public string City { get; set; }
+    }
+}
