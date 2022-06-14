ad608f83f7dd609f011469125051cbe7a32de360
Wu <1294039604@qq.com>
2022-06-14 16:38:06 +0800

    tijaioces

---
+++ C# ASP/Seven Day ASP.NET MVC/SevenDay/ASPMVC1/Controllers/ThreeController.Part5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPMVC1.Controllers
+{
+    public partial class ThreeController : System.Web.Mvc.Controller
+    {
+        public ActionResult ResetView()
+        {
+            return View(viewName: "ResetView"); 
+        }
+    }
+}
