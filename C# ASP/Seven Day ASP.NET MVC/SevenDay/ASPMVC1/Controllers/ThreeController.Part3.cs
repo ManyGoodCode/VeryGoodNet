8582c146e6ad4e06752aff8fd3ae2a0df5a37f3e
Wu <1294039604@qq.com>
2022-06-14 14:53:40 +0800

    隐藏窗体

---
+++ C# ASP/Seven Day ASP.NET MVC/SevenDay/ASPMVC1/Controllers/ThreeController.Part3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPMVC1.Controllers
+{
+    public partial class ThreeController : Controller
+    {
+        public ActionResult HideCancelFom()
+        {
+            return View(viewName: "HideCancel");
+        }
+
+        [HttpGet]
+        public string GetCancelClick()
+        {
+            return "this is Hide Cancel Click";
+        }
+    }
+}
