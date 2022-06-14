64e08ce427ee6030feec00181e3be70815e763e3
Wu <1294039604@qq.com>
2022-06-14 17:01:42 +0800

    模型绑定

--- C# ASP/Seven Day ASP.NET MVC/SevenDay/ASPMVC1/Controllers/ThreeController.Part6.cs
+++ C# ASP/Seven Day ASP.NET MVC/SevenDay/ASPMVC1/Controllers/ThreeController.Part6.cs
@@ -14,19 +14,42 @@ namespace ASPMVC1.Controllers
             return View(viewName: "ModelBind");
         }
 
-        // Post 表格绑定的方法1
-        public string Bind1(Person p)
+        // Post 表格绑定的方法1 当Post提交的Form 和 函数的参数名称一样时【但是与对象属性不一致】
+        public string Bind1(string ame, int alary)
         {
+            Person p = new Person()
+            {
+                Name = ame,
+                Salary = alary
+            };
+
             return string.Format("Bind1 Name:{0}  Salary:{1}", p.Name, p.Salary);
         }
 
 
-        // Post 表格绑定的方法2
+        // Post 表格绑定的方法2 当Post提交的Form 和 函数的参数名称一样时【但是与对象属性不一致】
         public string Bind2()
         {
-            string name = Request.Form["Name"];
-            string salary = Request.Form["Salary"];
+            string name = Request.Form["ame"];
+            string salary = Request.Form["alary"];
             return string.Format("Bind2 Name:{0}  Salary:{1}", name, salary);
+        }
+
+        // 自适配 DefaultModelBinder，将提交的字段映射到对象
+        public string Bind3([ModelBinder(typeof(PersonModelBinder))]Person p)
+        {
+            return string.Format("Bind3 Name:{0}  Salary:{1}", p.Name, p.Salary);
+        }
+    }
+
+    public class PersonModelBinder : DefaultModelBinder
+    {
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            Person p = new Person();
+            p.Name = controllerContext.HttpContext.Request.Form["ame"];
+            p.Salary = int.Parse(controllerContext.HttpContext.Request.Form["alary"]);
+            return p;
         }
     }
 }
