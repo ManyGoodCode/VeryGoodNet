b6928644862ae260e0d971ae3a0c4f9f3e84e34a
Wu <1294039604@qq.com>
2022-06-14 15:29:49 +0800

    修改

---
+++ C# ASP/Seven Day ASP.NET MVC/SevenDay/ASPMVC1/Controllers/ThreeController.Part4.cs
@@ -0,0 +1,27 @@
+﻿using ASPMVC1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPMVC1.Controllers
+{
+    public partial class ThreeController : System.Web.Mvc.Controller
+    {
+        public ActionResult LoadJavaScript()
+        {
+            return View(viewName: "LoadJavaScript");
+        }
+
+        public  string NowJavaScriptURL(Person p)
+        {
+            return string.Format("NowJavaScriptURL {0}{1}", p.Name, p.Salary);
+        }
+
+        public string ChangeJavaScriptURL(Person p)
+        {
+            return string.Format("ChangeJavaScriptURL {0}{1}", p.Name, p.Salary);
+        }
+    }
+}
