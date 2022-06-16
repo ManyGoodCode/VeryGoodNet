577fdb7c64a81006279d081a99f24e340e0eb2b0
Wu <1294039604@qq.com>
2022-06-16 17:30:02 +0800

    的

---
+++ C# ASP/Seven Day ASP.NET MVC/SevenDay/ASPMVC1/Controllers/FiveController.cs
@@ -0,0 +1,29 @@
+﻿using ASPMVC1.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPMVC1.Controllers
+{
+    public class FiveController : Controller
+    {
+        // 显示分布视图 ，将模型对应的值进行传输
+        // 分布视图放在 Share文件夹里面,创建的时候选择Partial.其无后台程序
+        // Footer.cshtml,
+        public ActionResult Index()
+        {
+            FiveMainViewModel viewModel = new FiveMainViewModel()
+            {
+                Text = "主界面",
+                Foot = new FooterViewModel() {
+                    CompanyName = "HonyWell",
+                    Year = "1999"
+                }
+            };
+
+            return View(viewName: "IndexView",model: viewModel);
+        }
+    }
+}
---
+++ C# ASP/Seven Day ASP.NET MVC/SevenDay/ASPMVC1/Models/ViewModel/FooterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPMVC1.Models.ViewModel
+{
+    public class FooterViewModel
+    {
+        public string CompanyName { get; set; }
+        public string Year { get; set; }
+    }
+
+    public class FiveMainViewModel
+    {
+        public FooterViewModel Foot { get; set; }
+        public string Text { get; set; }
+    }
+}
