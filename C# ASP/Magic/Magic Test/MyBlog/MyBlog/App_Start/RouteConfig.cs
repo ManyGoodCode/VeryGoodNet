121f8bd545985a7a4099b4da8981d5a1a34a59a4
Wu <1294039604@qq.com>
2022-06-19 17:18:47 +0800

    文章博客

--- C# ASP/Magic/Magic Test/MyBlog/MyBlog/App_Start/RouteConfig.cs
+++ C# ASP/Magic/Magic Test/MyBlog/MyBlog/App_Start/RouteConfig.cs
@@ -9,6 +9,9 @@ namespace MyBlog
 {
     public class RouteConfig
     {
+        // 这段代码是为MVC应用程序注册了一个路由，这个路由根据url所指的模板去匹配，然后映射到相应的Controller和Action上，
+        // 并且默认的Controller和Action是Home和Index,这也是为什么直接访问地址时会自动打开主页面的原因，而/Post/Get/1就代表了Controller是Post、Action
+        // 这也是为什么直接访问地址时会自动打开主页面的原因，而/Post/Get/1就代表了Controller是Post、Action
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
---
+++ C# ASP/Magic/Magic Test/MyBlog/MyBlog/Controllers/PostController.cs
@@ -0,0 +1,39 @@
+﻿using MyBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlog.Controllers
+{
+    public class PostController : Controller
+    {
+        private static List<Post> Posts = new List<Post>()
+        {
+            new Post(){ ID =1,Title ="文章1", Author = "Nicky,Wu", Content = "文章1的内容",CreateDate = DateTime.Now,ModifyDate= DateTime.Now },
+            new Post(){ ID =2,Title ="文章2", Author = "Nicky,Wu", Content = "文章2的内容",CreateDate = DateTime.Now,ModifyDate= DateTime.Now },
+            new Post(){ ID =3,Title ="文章3", Author = "Nicky,Wu", Content = "文章3的内容",CreateDate = DateTime.Now,ModifyDate= DateTime.Now },
+            new Post(){ ID =4,Title ="文章4", Author = "Nicky,Wu", Content = "文章4的内容",CreateDate = DateTime.Now,ModifyDate= DateTime.Now },
+            new Post(){ ID =5,Title ="文章5", Author = "Nicky,Wu", Content = "文章5的内容",CreateDate = DateTime.Now,ModifyDate= DateTime.Now }
+        };
+
+        public ActionResult Index()
+        {
+            ViewBag.Posts = Posts;
+            return View();
+        }
+
+        public ActionResult Get(int id)
+        {
+            Post post = Posts.Where(p => p.ID == id).FirstOrDefault();
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.Post = post;
+            return View();
+        }
+    }
+}
