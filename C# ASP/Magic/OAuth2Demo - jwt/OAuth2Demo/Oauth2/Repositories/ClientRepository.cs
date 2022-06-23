cb823f12bc6a9d5e3de6d03bb88579f82732c865
Wu <1294039604@qq.com>
2022-06-23 08:44:30 +0800

    提交

---
+++ C# ASP/Magic/OAuth2Demo - jwt/OAuth2Demo/Oauth2/Repositories/ClientRepository.cs
@@ -0,0 +1,24 @@
+﻿using OAuth2Demo.Oauth2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OAuth2Demo.Oauth2.Repositories
+{
+    public class ClientRepository
+    {
+        public static List<Client> Clients = new List<Client>() {
+            new Client{
+                 Id = "test1",
+                 RedirectUrl = "http://localhost:59273/",
+                 Secret = "LHZ5bUlOR2dzW1Yzd1dkbHdFbXNQSVBHSEs9dTZQKTE="
+            },
+            new Client{
+                 Id = "test2",
+                 RedirectUrl = "http://XXX.XXX.XXX/",
+                 Secret = "fnQuMmxXJX4rdXdSc0ZDQ0owOGJhQTM1TTZBN251XnQ="
+            }
+        };
+    }
+}
