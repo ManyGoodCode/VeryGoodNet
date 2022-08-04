fdece6abcebbc7eab35a443ee2ae934b5ea0c3eb
Wu <1294039604@qq.com>
2022-08-04 22:08:04 +0800

    qqq

--- Good frame/TinyIoC/LocalTest/F002438/F002438/Form1.cs
+++ Good frame/TinyIoC/LocalTest/F002438/F002438/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using F002438.Entity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +17,24 @@ namespace F002438
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //TinyIoCContainer.MultiRegisterOptions
+            TinyIoCContainer.Current.Register<ISt, St>();
+            ISt st = TinyIoCContainer.Current.Resolve<St>();
+            st.Age = 4;
+
+            ISt st1 = TinyIoCContainer.Current.Resolve<ISt>();
+        }
+    }
+
+    public class St : ISt
+    {
+        public override int Age { get; set; }
+    }
+    public abstract class ISt
+    {
+        public  virtual int Age { get; set; }
     }
 }
