eb821d97a1e1bd294236a8322c254d6596d7aeac
Wu <1294039604@qq.com>
2022-08-04 09:02:11 +0800

    钱钱钱

---
+++ Good frame/TinyIoC/LocalTest/F002438/F002438/Entity/SafeDictionary.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F002438.Entity
+{
+
+    public class SafeDictionary<TKey, TValue> : IDisposable
+    {
+        private readonly object lck = new object();
+        private readonly Dictionary<TKey, TValue> dictionary = new Dictionary<TKey, TValue>();
+
+        public TValue this[TKey key]
+        {
+            set
+            {
+                lock (lck)
+                {
+                    TValue current;
+                    if (dictionary.TryGetValue(key, out current))
+                    {
+                        IDisposable disposable = current as IDisposable;
+                        if (disposable != null)
+                            disposable.Dispose();
+                    }
+
+                    dictionary[key] = value;
+                }
+            }
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            lock (lck)
+            {
+                return dictionary.ContainsKey(key);
+            }
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            lock (lck)
+            {
+                return dictionary.TryGetValue(key, out value);
+            }
+        }
+
+        public bool Remove(TKey key)
+        {
+            lock (lck)
+            {
+                return dictionary.Remove(key);
+            }
+        }
+
+        public void Clear()
+        {
+            lock (lck)
+            {
+                dictionary.Clear();
+            }
+        }
+
+        public IEnumerable<TKey> Keys
+        {
+            get
+            {
+                return dictionary.Keys;
+            }
+        }
+
+        public void Dispose()
+        {
+            lock (lck)
+            {
+                IEnumerable<IDisposable> disposableItems = from item in dictionary.Values
+                                                           where item is IDisposable
+                                                           select item as IDisposable;
+
+                foreach (IDisposable item in disposableItems)
+                {
+                    item.Dispose();
+                }
+            }
+
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
