b5ed604d48b423bdac79a69c0cfdcd01751e16d0
Wu <1294039604@qq.com>
2022-08-07 16:19:27 +0800

    腐蚀膨胀

--- Good frame/OpenCV/WindowsFormsApp4/WindowsFormsApp4/Form3.Designer.cs
+++ Good frame/OpenCV/WindowsFormsApp4/WindowsFormsApp4/Form3.Designer.cs
@@ -28,11 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(322, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(870, 522);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1515, 709);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form3";
             this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Good frame/OpenCV/WindowsFormsApp4/WindowsFormsApp4/Form3.cs
+++ Good frame/OpenCV/WindowsFormsApp4/WindowsFormsApp4/Form3.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,6 +20,39 @@ namespace WindowsFormsApp4
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Mat srcImage = Cv2.ImRead("3.jpeg");
+
+            // 在窗口显示原图
+            Cv2.ImShow("原图", srcImage);
+
+            // 进行腐蚀操作
+            // Morph Shapes :形态学
+            Mat element = Cv2.GetStructuringElement(
+                shape: MorphShapes.Rect,       // 类型：腐蚀/膨胀
+                ksize: new OpenCvSharp.Size()  // 腐蚀和大小
+                {
+                    Width = 15,
+                    Height = 15
+                });
+
+            Mat dstImage = new Mat();
+            Cv2.Erode(
+                src: srcImage,
+                dst: dstImage,
+               element: element);
+
+
+            // 显示图片到Picture
+            Bitmap map = BitmapConverter.ToBitmap(dstImage);
+            pictureBox1.Image = map;
+            using (new Window("效果", dstImage))
+            {
+                Cv2.WaitKey();
+            }
         }
     }
 }
