b1d04713d040fe3eb787868f1ce9693528ef0829
Wu <1294039604@qq.com>
2022-08-07 15:56:01 +0800

    OpenCV

---
+++ Good frame/OpenCV/WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace WindowsFormsApp4
+{
+    /// <summary>
+    /// OpenCV 打开图像
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Mat srcImage = new Mat("1.jpg");
+            //此处通过窗体显示
+            Cv2.ImShow("Image", srcImage);
+            Cv2.WaitKey(0);
+
+            Bitmap bitmap = BitmapConverter.ToBitmap(srcImage);
+            pictureBox1.Image = bitmap;
+        }
+    }
+}
---
+++ Good frame/OpenCV/WindowsFormsApp4/WindowsFormsApp4/Form2.cs
@@ -0,0 +1,59 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    /// <summary>
+    /// OpenCV 图像合并
+    /// </summary>
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Mat image1 = Cv2.ImRead("2a.jpg");
+
+
+
+            Mat image2 = new Mat("2b.jpg");
+
+            // 设置图片2需要显示的区域
+            Mat imageROI = image1[new Rect()
+            {
+                X = 400,
+                Y = 400,
+                Height = image2.Rows,
+                Width = image2.Cols
+            }];
+
+            // 重合两张图片
+            Cv2.AddWeighted(imageROI, 0.7, image2, 0.3, 0.0, imageROI);
+            // 显示图片到Picture
+            Bitmap map = BitmapConverter.ToBitmap(image1);
+            pictureBox1.Image = map;
+            using (new Window("合并", image1))
+            {
+                Cv2.WaitKey();
+            }
+
+        }
+    }
+}
