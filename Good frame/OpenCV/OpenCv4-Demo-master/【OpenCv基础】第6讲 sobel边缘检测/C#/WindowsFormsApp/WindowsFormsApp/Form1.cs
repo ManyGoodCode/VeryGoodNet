5156a5fb4171fc5852a4782003532aed13f39a50
Wu <1294039604@qq.com>
2022-08-08 16:38:21 +0800

    边缘检测

--- Good frame/OpenCV/OpenCv4-Demo-master/【OpenCv基础】第6讲 sobel边缘检测/C#/WindowsFormsApp/WindowsFormsApp/Form1.cs
+++ Good frame/OpenCV/OpenCv4-Demo-master/【OpenCv基础】第6讲 sobel边缘检测/C#/WindowsFormsApp/WindowsFormsApp/Form1.cs
@@ -23,29 +23,35 @@ namespace WindowsFormsApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Mat grad_x = new Mat();
-            Mat grad_y = new Mat();
-            Mat abs_grad_x = new Mat(); 
-            Mat abs_grad_y = new Mat();
-            Mat dst = new Mat();
+            OpenCvSharp.Mat grad_x = new OpenCvSharp.Mat();
+            OpenCvSharp.Mat grad_y = new OpenCvSharp.Mat();
+            OpenCvSharp.Mat abs_grad_x = new OpenCvSharp.Mat(); 
+            OpenCvSharp.Mat abs_grad_y = new OpenCvSharp.Mat();
+            OpenCvSharp.Mat dst = new OpenCvSharp.Mat();
 
             //【1】读取图像
-            Mat src = Cv2.ImRead("1.jpg");
+            OpenCvSharp.Mat src = Cv2.ImRead("1.jpg");
+
             //【2】显示原图
-            Cv2.ImShow("原图", src);
+            OpenCvSharp.Cv2.ImShow("原图", src);
+
             //【3】求 X方向梯度
-            Cv2.Sobel(src, grad_x, MatType.CV_16S, 1, 0, 3, 1, 1, BorderTypes.Default);
-            Cv2.ConvertScaleAbs(grad_x, abs_grad_x);
-            Cv2.ImShow("【效果图】 X方向Sobel", abs_grad_x);
+            // Sobel:边缘检测
+            OpenCvSharp.Cv2.Sobel(src, grad_x, MatType.CV_16S, 1, 0, 3, 1, 1, OpenCvSharp.BorderTypes.Default);
+            OpenCvSharp.Cv2.ConvertScaleAbs(grad_x, abs_grad_x);
+            OpenCvSharp.Cv2.ImShow("【效果图】 X方向Sobel", abs_grad_x);
+
             //【4】求Y方向梯度
-            Cv2.Sobel(src, grad_y, MatType.CV_16S, 0, 1, 3, 1, 1, BorderTypes.Default);
-            Cv2.ConvertScaleAbs(grad_y, abs_grad_y);
-            Cv2.ImShow("【效果图】Y方向Sobel", abs_grad_y);
+            OpenCvSharp.Cv2.Sobel(src, grad_y, MatType.CV_16S, 0, 1, 3, 1, 1, OpenCvSharp.BorderTypes.Default);
+            OpenCvSharp.Cv2.ConvertScaleAbs(grad_y, abs_grad_y);
+            OpenCvSharp.Cv2.ImShow("【效果图】Y方向Sobel", abs_grad_y);
+
             //【5】合并梯度(近似)
-            Cv2.AddWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, dst);
-            Cv2.ImShow("【效果图】整体方向Sobel", dst);
+            OpenCvSharp.Cv2.AddWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, dst);
+            OpenCvSharp.Cv2.ImShow("【效果图】整体方向Sobel", dst);
+
             //【6】在pictureBox1中显示效果图
-            Bitmap map = BitmapConverter.ToBitmap(dst);
+            Bitmap map = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(dst);
             pictureBox1.Image = map;
         }
     }
