c98f4c236afcc9b49291fddbf802c65a86367805
Wu <1294039604@qq.com>
2022-08-08 17:00:41 +0800

    滤波器

--- Good frame/OpenCV/OpenCv4-Demo-master/【OpenCv基础】第8讲 Scharr滤波器/C#/WindowsFormsApp/WindowsFormsApp/Form1.cs
+++ Good frame/OpenCV/OpenCv4-Demo-master/【OpenCv基础】第8讲 Scharr滤波器/C#/WindowsFormsApp/WindowsFormsApp/Form1.cs
@@ -24,36 +24,36 @@ namespace WindowsFormsApp
         private void button1_Click(object sender, EventArgs e)
         {
             //【0】创建 grad_x 和 grad_y 矩阵
-            Mat grad_x = new Mat();
-            Mat grad_y = new Mat();
-            Mat abs_grad_x = new Mat();
-            Mat abs_grad_y = new Mat();
-            Mat dst = new Mat();
+            OpenCvSharp.Mat grad_x = new OpenCvSharp.Mat();
+            OpenCvSharp.Mat grad_y = new OpenCvSharp.Mat();
+            OpenCvSharp.Mat abs_grad_x = new OpenCvSharp.Mat();
+            OpenCvSharp.Mat abs_grad_y = new OpenCvSharp.Mat();
+            OpenCvSharp.Mat dst = new OpenCvSharp.Mat();
 
             // 【1】载入原始图
-            Mat src = Cv2.ImRead("1.jpg");
+            OpenCvSharp.Mat src = OpenCvSharp.Cv2.ImRead("1.jpg");
 
             // 【2】显示原图
-            Cv2.ImShow("原图", src);
+            OpenCvSharp.Cv2.ImShow("原图", src);
 
             // 【3】求 X方向梯度
-            Cv2.Scharr(src, grad_x, MatType.CV_16S, 1, 0, 1, 0, BorderTypes.Default);
-            Cv2.ConvertScaleAbs(grad_x, abs_grad_x);
-            Cv2.ImShow("【效果图】 X方向Scharr", abs_grad_x);
+            OpenCvSharp.Cv2.Scharr(src, grad_x, MatType.CV_16S, 1, 0, 1, 0, BorderTypes.Default);
+            OpenCvSharp.Cv2.ConvertScaleAbs(grad_x, abs_grad_x);
+            OpenCvSharp.Cv2.ImShow("【效果图】 X方向Scharr", abs_grad_x);
 
             // 【4】求 Y方向梯度
-            Cv2.Scharr(src, grad_y, MatType.CV_16S, 0, 1, 1, 0, BorderTypes.Default);
-            Cv2.ConvertScaleAbs(grad_y, abs_grad_y);
-            Cv2.ImShow("【效果图】 Y方向Scharr", abs_grad_y);
+            OpenCvSharp.Cv2.Scharr(src, grad_y, MatType.CV_16S, 0, 1, 1, 0, BorderTypes.Default);
+            OpenCvSharp.Cv2.ConvertScaleAbs(grad_y, abs_grad_y);
+            OpenCvSharp.Cv2.ImShow("【效果图】 Y方向Scharr", abs_grad_y);
 
             // 【5】合并梯度(近似)
-            Cv2.AddWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, dst);
+            OpenCvSharp.Cv2.AddWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, dst);
 
             // 【6】显示效果图 
-            Cv2.ImShow("【效果图】合并梯度后Scharr", dst);
+            OpenCvSharp.Cv2.ImShow("【效果图】合并梯度后Scharr", dst);
 
             // 【7】在pictureBox1中显示效果图
-            Bitmap map = BitmapConverter.ToBitmap(dst);
+            Bitmap map = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(dst);
             pictureBox1.Image = map;
         }
     }
