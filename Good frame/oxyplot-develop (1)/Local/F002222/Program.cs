5c90322e78545dbd18d62f1be00947062e80f0d3
Wu <1294039604@qq.com>
2022-08-13 16:22:04 +0800

    提交测试

---
+++ Good frame/oxyplot-develop (1)/Local/F002222/Program.cs
@@ -0,0 +1,29 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F002222
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            DataPoint dataPoint1 = new DataPoint(1,2);
+            DataPoint dataPoint2 = new DataPoint(double.NaN, 2);
+
+            bool result1 = dataPoint1.IsDefined();
+            bool result2 = dataPoint2.IsDefined();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
