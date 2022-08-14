7b6b8b6db1e93f00984bcbaf114b989015264092
Wu <1294039604@qq.com>
2022-08-14 21:57:16 +0800

    tij

--- Good frame/oxyplot-develop (1)/Local/Core/WindowsForms/WindowsFormsDemo/Form1.cs
+++ Good frame/oxyplot-develop (1)/Local/Core/WindowsForms/WindowsFormsDemo/Form1.cs
@@ -1,10 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Form1.cs" company="OxyPlot">
-//   Copyright (c) 2014 OxyPlot contributors
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-namespace WindowsFormsDemo
+﻿namespace WindowsFormsDemo
 {
     using System;
     using System.Drawing;
@@ -19,13 +13,14 @@ namespace WindowsFormsDemo
         {
             InitializeComponent();
             this.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
-            var pm = new PlotModel
+            PlotModel pm = new PlotModel
             {
                 Title = "Trigonometric functions",
                 Subtitle = "Example using the FunctionSeries",
                 PlotType = PlotType.Cartesian,
                 Background = OxyColors.White
             };
+
             pm.Series.Add(new FunctionSeries(Math.Sin, -10, 10, 0.1, "sin(x)"));
             pm.Series.Add(new FunctionSeries(Math.Cos, -10, 10, 0.1, "cos(x)"));
             pm.Series.Add(new FunctionSeries(t => 5 * Math.Cos(t), t => 5 * Math.Sin(t), 0, 2 * Math.PI, 0.1, "cos(t),sin(t)"));
--- Good frame/oxyplot-develop (1)/Local/Core/WindowsForms/WindowsFormsDemo/Program.cs
+++ Good frame/oxyplot-develop (1)/Local/Core/WindowsForms/WindowsFormsDemo/Program.cs
@@ -1,22 +1,10 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Program.cs" company="OxyPlot">
-//   Copyright (c) 2014 OxyPlot contributors
-// </copyright>
-// <summary>
-//   The main entry point for the application.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
-
-using System;
+﻿using System;
 using System.Windows.Forms;
 
 namespace WindowsFormsDemo
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
