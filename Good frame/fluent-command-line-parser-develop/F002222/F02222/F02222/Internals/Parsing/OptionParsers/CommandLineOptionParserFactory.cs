68e5efad070d6016849267ac577292a9540c6054
Wu <1294039604@qq.com>
2022-08-15 11:23:27 +0800

    提交

--- Good frame/fluent-command-line-parser-develop/F002222/F02222/F02222/Internals/Parsing/OptionParsers/CommandLineOptionParserFactory.cs
+++ Good frame/fluent-command-line-parser-develop/F002222/F02222/F02222/Internals/Parsing/OptionParsers/CommandLineOptionParserFactory.cs
@@ -1,40 +1,12 @@
-#region License
-// CommandLineOptionParserFactory.cs
-// Copyright (c) 2013, Simon Williams
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without modification, are permitted provide
-// d that the following conditions are met:
-// 
-// Redistributions of source code must retain the above copyright notice, this list of conditions and the
-// following disclaimer.
-// 
-// Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
-// the following disclaimer in the documentation and/or other materials provided with the distribution.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
-// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-// POSSIBILITY OF SUCH DAMAGE.
-#endregion
-
 using System;
 using System.Collections.Generic;
 
 namespace Fclp.Internals.Parsing.OptionParsers
 {
-    /// <summary>
-    /// 
-    /// </summary>
     public class CommandLineOptionParserFactory : ICommandLineOptionParserFactory
     {
-        /// <summary>
-        /// Initialises a new instance of the <see cref="CommandLineOptionParserFactory"/> class.
-        /// </summary>
+        internal Dictionary<Type, object> Parsers { get; set; }
+
         public CommandLineOptionParserFactory()
         {
             this.Parsers = new Dictionary<Type, object>();
@@ -62,37 +34,18 @@ namespace Fclp.Internals.Parsing.OptionParsers
             this.AddOrReplace(new NullableCommandLineOptionParser<TimeSpan>(this));
         }
 
-        internal Dictionary<Type, object> Parsers { get; set; }
-
-        /// <summary>
-        /// Adds the specified <see cref="ICommandLineOptionParser{T}"/> to this factories list of supported parsers.
-        /// If an existing parser has already been registered for the type then it will be replaced.
-        /// </summary>
-        /// <typeparam name="T">The type which the <see cref="ICommandLineOptionParser{T}"/> will be returned for.</typeparam>
-        /// <param name="parser">The parser to return for the specified type.</param>
-        /// <exception cref="ArgumentNullException">If <paramref name="parser"/> is <c>null</c>.</exception>
         public void AddOrReplace<T>(ICommandLineOptionParser<T> parser)
         {
-            if (parser == null) throw new ArgumentNullException("parser");
-
-            var parserType = typeof(T);
-
-            // remove existing
+            if (parser == null)
+                throw new ArgumentNullException("parser");
+            Type parserType = typeof(T);
             this.Parsers.Remove(parserType);
-
             this.Parsers.Add(parserType, parser);
         }
 
-        /// <summary>
-        /// Creates a <see cref="ICommandLineOptionParser{T}"/> to handle the specified type.
-        /// </summary>
-        /// <typeparam name="T">The type of parser to create.</typeparam>
-        /// <returns>A <see cref="ICommandLineOptionParser{T}"/> suitable for the specified type.</returns>
-        /// <exception cref="UnsupportedTypeException">If the specified type is not supported by this factory.</exception>
         public ICommandLineOptionParser<T> CreateParser<T>()
         {
-            var type = typeof(T);
-
+            Type type = typeof(T);
             if (!this.Parsers.ContainsKey(type))
             {
                 if (!TryAddAsSpecialParser<T>(type))
@@ -104,21 +57,16 @@ namespace Fclp.Internals.Parsing.OptionParsers
             return (ICommandLineOptionParser<T>)this.Parsers[type];
         }
 
-        /// <summary>
-        /// Attempts to add a special case parser, such as Enum or List{TEnum} parser.
-        /// </summary>
-        /// <returns>True if a special parser was added for the type; otherwise false.</returns>
         private bool TryAddAsSpecialParser<T>(Type type)
         {
             if (type.IsEnum)
             {
                 bool hasFlags = typeof(T).IsDefined(typeof(FlagsAttribute), false);
-
                 Type enumParserType = hasFlags ?
                     typeof(EnumFlagCommandLineOptionParser<T>) :
                     typeof(EnumCommandLineOptionParser<T>);
 
-                if (!this.Parsers.ContainsKey(enumParserType))
+                if (!Parsers.ContainsKey(enumParserType))
                 {
                     if (hasFlags)
                     {
@@ -135,15 +83,13 @@ namespace Fclp.Internals.Parsing.OptionParsers
 
             if (type.IsGenericType)
             {
-                var genericType = TryGetListGenericType(type);
-
+                Type genericType = TryGetListGenericType(type);
                 if (genericType != null)
                 {
                     if (genericType.IsEnum || IsNullableEnum(genericType))
                     {
-                        var enumListParserType = typeof(ListCommandLineOptionParser<>).MakeGenericType(genericType);
-                        var parser = (ICommandLineOptionParser<T>)Activator.CreateInstance(enumListParserType, this);
-
+                        Type enumListParserType = typeof(ListCommandLineOptionParser<>).MakeGenericType(genericType);
+                        ICommandLineOptionParser<T> parser = (ICommandLineOptionParser<T>)Activator.CreateInstance(enumListParserType, this);
                         if (!this.Parsers.ContainsKey(type))
                         {
                             this.AddOrReplace(parser);
@@ -156,9 +102,9 @@ namespace Fclp.Internals.Parsing.OptionParsers
 
             if (IsNullableEnum(type))
             {
-                var underlyingType = Nullable.GetUnderlyingType(type);
-                var nullableEnumParserType = typeof(NullableEnumCommandLineOptionParser<>).MakeGenericType(underlyingType);
-                var parser = (ICommandLineOptionParser<T>)Activator.CreateInstance(nullableEnumParserType, this);
+                Type underlyingType = Nullable.GetUnderlyingType(type);
+                Type nullableEnumParserType = typeof(NullableEnumCommandLineOptionParser<>).MakeGenericType(underlyingType);
+                ICommandLineOptionParser<T> parser = (ICommandLineOptionParser<T>)Activator.CreateInstance(nullableEnumParserType, this);
 
                 if (!this.Parsers.ContainsKey(type))
                 {
@@ -177,9 +123,6 @@ namespace Fclp.Internals.Parsing.OptionParsers
             return (u != null) && u.IsEnum;
         }
 
-        /// <summary>
-        /// Attemps to get the type of generic from a generic list.
-        /// </summary>
         private static Type TryGetListGenericType(Type type)
         {
             if (type.IsGenericType && type.GetGenericTypeDefinition()
