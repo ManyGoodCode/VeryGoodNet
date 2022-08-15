6b38eee7b4059beeca93ed1a75d3e48350fa27e7
Wu <1294039604@qq.com>
2022-08-15 14:56:15 +0800

    tijaioces

--- Good frame/fluent-command-line-parser-develop/F002222/F02222/F002222.Application/Program.cs
+++ Good frame/fluent-command-line-parser-develop/F002222/F02222/F002222.Application/Program.cs
@@ -4,7 +4,6 @@ namespace Fclp.Examples
 {
     class CommandProgram
     {
-        // Contains all arguments for the add command
         class AddArgs
         {
             public bool Verbose { get; set; }
@@ -13,7 +12,6 @@ namespace Fclp.Examples
             public List<string> Files2 { get; set; }
         }
 
-        // Contains all arguments for the remove command
         class RemoveArgs
         {
             public bool Verbose { get; set; }
@@ -22,13 +20,10 @@ namespace Fclp.Examples
 
         static void Main(string[] args)
         {
-            var fclp = new FluentCommandLineParser();
+            FluentCommandLineParser fclp = new FluentCommandLineParser();
+            ICommandLineCommandFluent<AddArgs> addCmd = fclp.SetupCommand<AddArgs>("add")
+                             .OnSuccess(Add);
 
-            // use new SetupCommand method to initialise a command
-            var addCmd = fclp.SetupCommand<AddArgs>("add")
-                             .OnSuccess(Add); // executed when the add command is used
-
-            // the standard fclp framework, except against the created command rather than the fclp itself
             addCmd.Setup(addArgs => addArgs.Verbose)
                   .As('v', "verbose")
                   .SetDefault(false)
@@ -40,13 +35,13 @@ namespace Fclp.Examples
                   .WithDescription("If some files could not be added, do not abort");
 
             addCmd.Setup(addArgs => addArgs.Files)
-                  .As('f', "files")
+                  .As('f', "files") 
                   .WithDescription("Files to be tracked")
                   .UseForOrphanArguments();
 
-            // add the remove command
-            var remCmd = fclp.SetupCommand<RemoveArgs>("rem")
-                             .OnSuccess(Remove); // executed when the remove command is used
+            ICommandLineCommandFluent<RemoveArgs> remCmd = 
+                fclp.SetupCommand<RemoveArgs>("rem")
+                .OnSuccess(Remove); 
 
             remCmd.Setup(removeArgs => removeArgs.Verbose)
                      .As('v', "verbose")
@@ -63,12 +58,12 @@ namespace Fclp.Examples
 
         static void Add(AddArgs args)
         {
-            // add was executed
+            
         }
 
         static void Remove(RemoveArgs args)
         {
-            // remove was executed
+            
         }
     }
 }
