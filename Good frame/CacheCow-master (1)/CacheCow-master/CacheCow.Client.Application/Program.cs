b9f97125a49b4a1d8f641f6c2e5c5e9d45965fa8
Wu <1294039604@qq.com>
2022-08-17 18:43:45 +0800

    删除

---
+++ Good frame/CacheCow-master (1)/CacheCow-master/CacheCow.Client.Application/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CacheCow.Client.FileCacheStore;
+using CacheCow.Common;
+
+namespace CacheCow.Client.Application
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            FileStore fileStore = new FileStore("cache");
+            fileStore.AddOrUpdateAsync(new CacheKey("www.baidu.com", new string[2] { "111","333"}), new System.Net.Http.HttpResponseMessage());
+
+            //fileStore.TryRemoveAsync(new CacheKey("www.baidu.com", new string[2] { "111", "333" }));
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+        }
+    }
+}
