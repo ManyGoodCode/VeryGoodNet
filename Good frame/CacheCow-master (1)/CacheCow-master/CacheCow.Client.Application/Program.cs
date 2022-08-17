359ff646072efda916b6afd66f1fa6f38ccab521
Wu <1294039604@qq.com>
2022-08-17 19:14:44 +0800

    帮助

--- Good frame/CacheCow-master (1)/CacheCow-master/CacheCow.Client.Application/Program.cs
+++ Good frame/CacheCow-master (1)/CacheCow-master/CacheCow.Client.Application/Program.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using CacheCow.Client.FileCacheStore;
 using CacheCow.Common;
+using CacheCow.Common.Helpers;
 
 namespace CacheCow.Client.Application
 {
@@ -16,11 +17,14 @@ namespace CacheCow.Client.Application
         [STAThread]
         static void Main()
         {
-            FileStore fileStore = new FileStore("cache");
-            fileStore.AddOrUpdateAsync(new CacheKey("www.baidu.com", new string[2] { "111","333"}), new System.Net.Http.HttpResponseMessage());
-
+            //FileStore fileStore = new FileStore("cache");
+            //fileStore.AddOrUpdateAsync(new CacheKey("www.baidu.com", new string[2] { "111","333"}), new System.Net.Http.HttpResponseMessage());
             //fileStore.TryRemoveAsync(new CacheKey("www.baidu.com", new string[2] { "111", "333" }));
 
+
+            byte[] datas = new byte[3] { 0x11, 0x22, 0x34 };
+            string hexString = datas.ToHex();
+            byte[] datas1 = hexString.FromHex();
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
--- Good frame/CacheCow-master (1)/CacheCow-master/src/CacheCow.Common/Helpers/BasicExtensions.cs
+++ Good frame/CacheCow-master (1)/CacheCow-master/src/CacheCow.Common/Helpers/BasicExtensions.cs
@@ -8,6 +8,9 @@ namespace CacheCow.Common.Helpers
 {
 	public static class BasicExtensions
 	{
+        /// <summary>
+        /// 判断一个元素是否在集合中
+        /// </summary>
 		public static bool IsIn<T>(this T item, params T[] list)
 		{
 			if (list == null || list.Length == 0)
@@ -15,6 +18,9 @@ namespace CacheCow.Common.Helpers
 			return list.Any(x => EqualityComparer<T>.Default.Equals(x, item));
 		}
 
+        /// <summary>
+        /// 将委托集合变成一个委托
+        /// </summary>
 		public static Action Chain(this IEnumerable<Action> actions)
 		{
 			return () =>
@@ -24,6 +30,9 @@ namespace CacheCow.Common.Helpers
 			};
 		}
 
+        /// <summary>
+        /// 将异步委托集合变成一个异步委托
+        /// </summary>
         public static Func<Task> Chain(this IEnumerable<Func<Task>> actions)
         {
             return async () =>
@@ -33,6 +42,9 @@ namespace CacheCow.Common.Helpers
             };
         }
 
+        /// <summary>
+        /// 输出十六进制字符串 0x11 表示11
+        /// </summary>
 		public static string ToHex(this byte[] data)
 		{
             StringBuilder hex = new StringBuilder(data.Length * 2);
@@ -41,6 +53,9 @@ namespace CacheCow.Common.Helpers
             return hex.ToString();
         }
 
+        /// <summary>
+        /// 将字符串转换为十六进制数据 11 表示 0x11
+        /// </summary>
 		public static byte[] FromHex(this string hex)
 		{
             int NumberChars = hex.Length;
