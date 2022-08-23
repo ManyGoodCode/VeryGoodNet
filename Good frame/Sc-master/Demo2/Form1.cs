994d1e89ae955bb26e828d8bfc2b7f01ee35c023
Wu <1294039604@qq.com>
2022-08-23 13:31:27 +0800

    TIJIAO

---
+++ Good frame/Sc-master/Demo2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using Sc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo2
+{
+    public partial class Form1 : Form
+    {
+        ScMgr scMgr;
+        ScLayer root;
+        App app;
+        UpdateLayerFrm frm;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadModel1();
+        }
+
+        private void LoadModel1()
+        {
+            //////////////////////////////////////////
+            //透明无边框窗口测试代码，测试时打开下面的注释
+            scMgr = new ScMgr(stdControl: null, isUsedUpdateLayerFrm: true);
+            scMgr.BackgroundColor = Color.FromArgb(alpha: 100, red: 0, green: 0, blue: 251);
+            app = new App(scMgr: scMgr);
+            scMgr.ReBulid();
+            scMgr.Show();
+        }
+
+        private void LoadModel2()
+        {
+            ////////////////////////////////////
+            //常规窗口测试代码，测试时打开下面的注释
+            scMgr = new ScMgr(stdControl: panel);
+            scMgr.BackgroundColor = Color.FromArgb(alpha: 255,red: 246,green: 245,blue: 251);
+            app = new App(scMgr: scMgr);
+            scMgr.ReBulid();
+        }
+
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (scMgr != null)
+                scMgr.Refresh();
+        }
+    }
+}
---
+++ Good frame/Sc-master/demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demo
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            SetProcessDPIAware();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+
+        [DllImport("user32.dll")]
+        internal static extern bool SetProcessDPIAware();
+    }
+}
