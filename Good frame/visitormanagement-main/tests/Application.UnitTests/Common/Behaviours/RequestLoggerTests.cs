86c2ba163c2928f0076f2b7f6e4e017e2360eab7
Wu <1294039604@qq.com>
2022-09-05 20:58:06 +0800

    tijiaoces

--- Good frame/visitormanagement-main/tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
+++ Good frame/visitormanagement-main/tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
@@ -11,26 +11,26 @@ namespace CleanArchitecture.Blazor.Application.UnitTests.Common.Behaviours
 {
     public class RequestLoggerTests
     {
-        private readonly Mock<ICurrentUserService> _currentUserService;
-        private readonly Mock<IIdentityService> _identityService;
+        private readonly Mock<ICurrentUserService> currentUserService;
+        private readonly Mock<IIdentityService> identityService;
 
         public RequestLoggerTests()
         {
-            _currentUserService = new Mock<ICurrentUserService>();
-            _identityService = new Mock<IIdentityService>();
+            currentUserService = new Mock<ICurrentUserService>();
+            identityService = new Mock<IIdentityService>();
         }
 
         [Test]
         public async Task ShouldCallGetUserNameAsyncOnceIfAuthenticated()
         {
-            _currentUserService.Setup(x => x.UserId()).Returns(Task.FromResult("Administrator"));
-            _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<string>()), Times.Once);
+            currentUserService.Setup(x => x.UserId()).Returns(Task.FromResult("Administrator"));
+            identityService.Verify(i => i.GetUserNameAsync(It.IsAny<string>()), Times.Once);
         }
 
         [Test]
         public async Task ShouldNotCallGetUserNameAsyncOnceIfUnauthenticated()
         {
-            _identityService.Verify(i => i.GetUserNameAsync(null), Times.Never);
+            identityService.Verify(i => i.GetUserNameAsync(null), Times.Never);
         }
     }
 }
--- Good frame/visitormanagement-main/tests/Application.UnitTests/Common/Exceptions/ValidationExceptionTests.cs
+++ Good frame/visitormanagement-main/tests/Application.UnitTests/Common/Exceptions/ValidationExceptionTests.cs
@@ -7,7 +7,9 @@ using System.Collections.Generic;
 
 namespace CleanArchitecture.Blazor.Application.UnitTests.Common.Exceptions
 {
-
+    /// <summary>
+    /// 失败验证器的单元测试
+    /// </summary>
     public class ValidationExceptionTests
     {
         [Test]
@@ -18,10 +20,13 @@ namespace CleanArchitecture.Blazor.Application.UnitTests.Common.Exceptions
                 new ValidationFailure("Age", "'Age' must be over 18"),
             };
 
-            List<string> actual = new ValidationException(failures).ErrorMessages;
+            List<string> actual = new ValidationException(failures: failures).ErrorMessages;
             actual.Should().BeEquivalentTo(new List<string>() { "'Age' must be over 18" });
         }
 
+        /// <summary>
+        /// ValidationException 内部已经实现了对集合属性的 GroupBy / Select
+        /// </summary>
         [Test]
         public void MulitpleValidationFailureForMultiplePropertiesCreatesAMultipleElementErrorDictionaryEachWithMultipleValues()
         {
@@ -35,8 +40,8 @@ namespace CleanArchitecture.Blazor.Application.UnitTests.Common.Exceptions
                 new ValidationFailure("Password", "'Password' must contain lower case letter"),
             };
 
-            List<string> actual = new ValidationException(failures).ErrorMessages;
-            actual.Count.Should().Equals(2);
+            List<string> actual = new ValidationException(failures: failures).ErrorMessages;
+            actual.Count.Should().Be(2);
             actual.Should().Contain(r => r.Equals("'Age' must be 18 or older, 'Age' must be 25 or younger"));
         }
     }
