86f07b81ec842ea8fe77fa0563cb6d614e272709
Wu <1294039604@qq.com>
2022-09-07 23:25:29 +0800

    本地化

--- Good frame/visitormanagement-main/src/Application/Common/Helper/ConstantStringLocalizer.cs
+++ Good frame/visitormanagement-main/src/Application/Common/Helper/ConstantStringLocalizer.cs
@@ -3,6 +3,10 @@ using System.Resources;
 
 namespace CleanArchitecture.Blazor.Application.Common.Helper
 {
+    /// <summary>
+    /// 根据静态构造器初始化  静态，只读的本地对象
+    /// 通过调用静态 Localize(string key) 函数实现本地化
+    /// </summary>
     public static class ConstantStringLocalizer
     {
         public const string CONSTANTSTRINGRESOURCEID = "CleanArchitecture.Blazor.Application.Resources.Constants.ConstantString";
@@ -10,7 +14,7 @@ namespace CleanArchitecture.Blazor.Application.Common.Helper
 
         static ConstantStringLocalizer()
         {
-            rm = new ResourceManager(CONSTANTSTRINGRESOURCEID, typeof(ConstantStringLocalizer).Assembly);
+            rm = new ResourceManager(baseName: CONSTANTSTRINGRESOURCEID, assembly: typeof(ConstantStringLocalizer).Assembly);
         }
         public static string Localize(string key)
         {
