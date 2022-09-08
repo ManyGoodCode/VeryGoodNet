afaa48c77f4752a6243647517aca8f0705d79969
Wu <1294039604@qq.com>
2022-09-08 10:19:25 +0800

    规格表达式和缓存键值

--- Good frame/visitormanagement-main/src/Application/Common/Interfaces/ISpecification.cs
+++ Good frame/visitormanagement-main/src/Application/Common/Interfaces/ISpecification.cs
@@ -5,13 +5,35 @@ using CleanArchitecture.Blazor.Domain.Common;
 
 namespace CleanArchitecture.Blazor.Application.Common.Interfaces
 {
-
+    /// <summary>
+    /// 规格接口【也就是定义满足条件的一系列规格】包括如下：
+    /// 1. 标准规格表达式
+    /// 2. 包含规格集表达式
+    /// 3. 添加‘且’规格表达式的函数契约
+    /// 4. 添加‘或’规格表达式的函数契约
+    /// </summary>
     public interface ISpecification<T> where T : class, IEntity
     {
+        /// <summary>
+        /// 标准规格
+        /// </summary>
         Expression<Func<T, bool>> Criteria { get; }
+
+        /// <summary>
+        /// 包含规格集
+        /// </summary>
         List<Expression<Func<T, object>>> Includes { get; }
+
         List<string> IncludeStrings { get; }
+
+        /// <summary>
+        /// 添加‘且’规格的函数契约
+        /// </summary>
         Expression<Func<T, bool>> And(Expression<Func<T, bool>> query);
+
+        /// <summary>
+        /// 添加‘或’规格的函数契约
+        /// </summary>
         Expression<Func<T, bool>> Or(Expression<Func<T, bool>> query);
     }
 }
--- Good frame/visitormanagement-main/src/Application/Features/ApprovalHistories/Caching/ApprovalHistoryCacheKey.cs
+++ Good frame/visitormanagement-main/src/Application/Features/ApprovalHistories/Caching/ApprovalHistoryCacheKey.cs
@@ -5,27 +5,32 @@ using Microsoft.Extensions.Primitives;
 
 namespace CleanArchitecture.Blazor.Application.Features.ApprovalHistories.Caching
 {
-
+    /// <summary>
+    /// 1. 定义缓存键值。如：所有缓存键值，页面访问键值(传入页面名称参数获取)，访问者Id键值(传入访问者Id)
+    /// 2. 设置缓存最长事件3小时以及刷新缓存时间
+    /// </summary>
     public static class ApprovalHistoryCacheKey
     {
         public const string GetAllCacheKey = "all-ApprovalHistories";
         public static string GetPagtionCacheKey(string parameters) => $"ApprovalHistoriesWithPaginationQuery,{parameters}";
-
         public static string GetByVisitorIdCacheKey(int id) => $"GetByVisitorIdCacheKey,{id}";
+        private static System.Threading.CancellationTokenSource tokensource;
 
         static ApprovalHistoryCacheKey()
         {
-            _tokensource = new CancellationTokenSource(new TimeSpan(3, 0, 0));
+            tokensource = new CancellationTokenSource(new System.TimeSpan(hours: 3, minutes: 0, seconds: 0));
         }
-        private static CancellationTokenSource _tokensource;
+
         public static CancellationTokenSource SharedExpiryTokenSource()
         {
-            if (_tokensource.IsCancellationRequested)
+            if (tokensource.IsCancellationRequested)
             {
-                _tokensource = new CancellationTokenSource(new TimeSpan(3, 0, 0));
+                tokensource = new CancellationTokenSource(new System.TimeSpan(3, 0, 0));
             }
-            return _tokensource;
+
+            return tokensource;
         }
+
         public static MemoryCacheEntryOptions MemoryCacheEntryOptions => new MemoryCacheEntryOptions().AddExpirationToken(new CancellationChangeToken(SharedExpiryTokenSource().Token));
     }
 }
