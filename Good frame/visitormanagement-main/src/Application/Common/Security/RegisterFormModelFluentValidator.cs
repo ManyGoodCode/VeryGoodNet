3fbe01e8de0898e2afa713f7d2f1262ce2af99d4
Wu <1294039604@qq.com>
2022-09-10 22:01:48 +0800

    注册用户表单的验证器

--- Good frame/visitormanagement-main/src/Application/Common/Security/RegisterFormModelFluentValidator.cs
+++ Good frame/visitormanagement-main/src/Application/Common/Security/RegisterFormModelFluentValidator.cs
@@ -7,7 +7,7 @@ using FluentValidation;
 namespace CleanArchitecture.Blazor.Application.Common.Security
 {
     /// <summary>
-    /// 注册用户表单模型验证�?
+    /// 注册用户表单的验证器
     /// </summary>
     public class RegisterFormModelFluentValidator : AbstractValidator<RegisterFormModel>
     {
@@ -15,18 +15,18 @@ namespace CleanArchitecture.Blazor.Application.Common.Security
         {
             RuleFor(x => x.UserName)
                 .NotEmpty()
-                .Length(2, 100);
+                .Length(min: 2, max: 100);
             RuleFor(x => x.Email)
                 .NotEmpty()
-                .MaximumLength(255)
+                .MaximumLength(maximumLength: 255)
                 .EmailAddress();
-            RuleFor(p => p.Password).NotEmpty().WithMessage("Your password cannot be empty")
-                      .MinimumLength(6).WithMessage("Your password length must be at least 6.")
-                      .MaximumLength(16).WithMessage("Your password length must not exceed 16.")
-                      .Matches(@"[A-Z]+").WithMessage("Your password must contain at least one uppercase letter.")
-                      .Matches(@"[a-z]+").WithMessage("Your password must contain at least one lowercase letter.")
-                      .Matches(@"[0-9]+").WithMessage("Your password must contain at least one number.")
-                      .Matches(@"[\!\?\*\.]+").WithMessage("Your password must contain at least one (!? *.).");
+            RuleFor(p => p.Password).NotEmpty().WithMessage(errorMessage: "Your password cannot be empty")
+                      .MinimumLength(6).WithMessage(errorMessage: "Your password length must be at least 6.")
+                      .MaximumLength(16).WithMessage(errorMessage: "Your password length must not exceed 16.")
+                      .Matches(@"[A-Z]+").WithMessage(errorMessage: "Your password must contain at least one uppercase letter.")
+                      .Matches(@"[a-z]+").WithMessage(errorMessage: "Your password must contain at least one lowercase letter.")
+                      .Matches(@"[0-9]+").WithMessage(errorMessage: "Your password must contain at least one number.")
+                      .Matches(@"[\!\?\*\.]+").WithMessage(errorMessage: "Your password must contain at least one (!? *.).");
             RuleFor(x => x.ConfirmPassword)
                  .Equal(x => x.Password);
             RuleFor(x => x.AgreeToTerms)
@@ -35,7 +35,8 @@ namespace CleanArchitecture.Blazor.Application.Common.Security
 
         public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
         {
-            FluentValidation.Results.ValidationResult result = await ValidateAsync(ValidationContext<RegisterFormModel>.CreateWithOptions((RegisterFormModel)model, x => x.IncludeProperties(propertyName)));
+            FluentValidation.Results.ValidationResult result = await ValidateAsync(ValidationContext<RegisterFormModel>
+                .CreateWithOptions((RegisterFormModel)model, x => x.IncludeProperties(propertyName)));
             if (result.IsValid)
                 return Array.Empty<string>();
             return result.Errors.Select(e => e.ErrorMessage);
