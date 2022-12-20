2bcc6df19a225aa651cfbc1ac130a8f942ddec39
ManyGoodCode <50822221+ManyGoodCode@users.noreply.github.com>
2022-12-20 23:28:11 +0800

    11

--- Good frame/oxyplot-develop (1)/Local/OxyPlot/Axes/CategoryColorAxis.cs
+++ Good frame/oxyplot-develop (1)/Local/OxyPlot/Axes/CategoryColorAxis.cs
@@ -1,47 +1,18 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="CategoryColorAxis.cs" company="OxyPlot">
-//   Copyright (c) 2014 OxyPlot contributors
-// </copyright>
-// <summary>
-//   Represents a categorized color axis.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
 
 namespace OxyPlot.Axes
 {
     using System;
     using System.Collections.Generic;
 
-    /// <summary>
-    /// Represents a categorized color axis.
-    /// </summary>
     public class CategoryColorAxis : CategoryAxis, IColorAxis
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CategoryColorAxis" /> class.
-        /// </summary>
         public CategoryColorAxis()
         {
             this.Palette = new OxyPalette();
         }
-
-        /// <summary>
-        /// Gets or sets the invalid category color.
-        /// </summary>
-        /// <value>The color.</value>
+        
         public OxyColor InvalidCategoryColor { get; set; }
-
-        /// <summary>
-        /// Gets or sets the palette.
-        /// </summary>
-        /// <value>The palette.</value>
         public OxyPalette Palette { get; set; }
-
-        /// <summary>
-        /// Gets the color of the specified index in the color palette.
-        /// </summary>
-        /// <param name="paletteIndex">The color map index (less than NumberOfEntries).</param>
-        /// <returns>The color.</returns>
         public OxyColor GetColor(int paletteIndex)
         {
             if (paletteIndex == -1)
@@ -57,22 +28,12 @@ namespace OxyPlot.Axes
             return this.Palette.Colors[paletteIndex];
         }
 
-        /// <summary>
-        /// Gets the palette index of the specified value.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>The palette index.</returns>
-        /// <remarks>If the value is less than minimum, 0 is returned. If the value is greater than maximum, Palette.Colors.Count+1 is returned.</remarks>
         public int GetPaletteIndex(double value)
         {
             return (int)value;
         }
 
-        /// <summary>
-        /// Renders the axis on the specified render context.
-        /// </summary>
-        /// <param name="rc">The render context.</param>
-        /// <param name="pass">The pass.</param>
+
         public override void Render(IRenderContext rc, int pass)
         {
             if (this.Position == AxisPosition.None)
@@ -138,11 +99,7 @@ namespace OxyPlot.Axes
             base.Render(rc, pass);
         }
 
-        /// <summary>
-        /// Gets the high value of the specified palette index.
-        /// </summary>
-        /// <param name="paletteIndex">Index of the palette.</param>
-        /// <returns>The value.</returns>
+
         protected double GetHighValue(int paletteIndex)
         {
             IList<double> majorLabelValues;
@@ -153,12 +110,6 @@ namespace OxyPlot.Axes
             return highValue;
         }
 
-        /// <summary>
-        /// Gets the high value.
-        /// </summary>
-        /// <param name="paletteIndex">Index of the palette.</param>
-        /// <param name="majorLabelValues">The major label values.</param>
-        /// <returns>The value.</returns>
         private double GetHighValue(int paletteIndex, IList<double> majorLabelValues)
         {
             double highValue = paletteIndex >= this.Palette.Colors.Count - 1
@@ -167,12 +118,6 @@ namespace OxyPlot.Axes
             return highValue;
         }
 
-        /// <summary>
-        /// Gets the low value.
-        /// </summary>
-        /// <param name="paletteIndex">Index of the palette.</param>
-        /// <param name="majorLabelValues">The major label values.</param>
-        /// <returns>The value.</returns>
         private double GetLowValue(int paletteIndex, IList<double> majorLabelValues)
         {
             double lowValue = paletteIndex == 0
