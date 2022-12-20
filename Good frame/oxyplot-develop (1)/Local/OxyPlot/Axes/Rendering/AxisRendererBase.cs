3704068ea308f07d901bd47b1ab7b2bedbb8bcec
ManyGoodCode <50822221+ManyGoodCode@users.noreply.github.com>
2022-12-20 23:01:55 +0800

    11

--- Good frame/oxyplot-develop (1)/Local/OxyPlot/Axes/Rendering/AxisRendererBase.cs
+++ Good frame/oxyplot-develop (1)/Local/OxyPlot/Axes/Rendering/AxisRendererBase.cs
@@ -1,61 +1,21 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AxisRendererBase.cs" company="OxyPlot">
-//   Copyright (c) 2014 OxyPlot contributors
-// </copyright>
-// <summary>
-//   Provides an abstract base class for axis renderers.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
 
 namespace OxyPlot.Axes
 {
     using System.Collections.Generic;
 
-    /// <summary>
-    /// Provides an abstract base class for axis renderers.
-    /// </summary>
     public abstract class AxisRendererBase
     {
-        /// <summary>
-        /// The plot.
-        /// </summary>
         private readonly PlotModel plot;
-
-        /// <summary>
-        /// The render context.
-        /// </summary>
         private readonly IRenderContext rc;
-
-        /// <summary>
-        /// The major label values
-        /// </summary>
         private IList<double> majorLabelValues;
-
-        /// <summary>
-        /// The major tick values
-        /// </summary>
         private IList<double> majorTickValues;
-
-        /// <summary>
-        /// The minor tick values
-        /// </summary>
         private IList<double> minorTickValues;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AxisRendererBase" /> class.
-        /// </summary>
-        /// <param name="rc">The render context.</param>
-        /// <param name="plot">The plot.</param>
         protected AxisRendererBase(IRenderContext rc, PlotModel plot)
         {
             this.plot = plot;
             this.rc = rc;
         }
-
-        /// <summary>
-        /// Gets the plot.
-        /// </summary>
-        /// <value>The plot.</value>
+        
         protected PlotModel Plot
         {
             get
@@ -64,10 +24,6 @@ namespace OxyPlot.Axes
             }
         }
 
-        /// <summary>
-        /// Gets the render context.
-        /// </summary>
-        /// <value>The render context.</value>
         protected IRenderContext RenderContext
         {
             get
@@ -76,19 +32,8 @@ namespace OxyPlot.Axes
             }
         }
 
-        /// <summary>
-        /// Gets or sets the axis lines pen.
-        /// </summary>
         protected OxyPen AxislinePen { get; set; }
-
-        /// <summary>
-        /// Gets or sets the extra grid lines pen.
-        /// </summary>
         protected OxyPen ExtraPen { get; set; }
-
-        /// <summary>
-        /// Gets or sets the major label values.
-        /// </summary>
         protected IList<double> MajorLabelValues
         {
             get
@@ -101,20 +46,9 @@ namespace OxyPlot.Axes
                 this.majorLabelValues = value;
             }
         }
-
-        /// <summary>
-        /// Gets or sets the major grid lines pen.
-        /// </summary>
+        
         protected OxyPen MajorPen { get; set; }
-
-        /// <summary>
-        /// Gets or sets the major tick pen.
-        /// </summary>
         protected OxyPen MajorTickPen { get; set; }
-
-        /// <summary>
-        /// Gets or sets the major tick values.
-        /// </summary>
         protected IList<double> MajorTickValues
         {
             get
@@ -128,19 +62,8 @@ namespace OxyPlot.Axes
             }
         }
 
-        /// <summary>
-        /// Gets or sets the minor grid lines pen.
-        /// </summary>
         protected OxyPen MinorPen { get; set; }
-
-        /// <summary>
-        /// Gets or sets the minor tick pen.
-        /// </summary>
         protected OxyPen MinorTickPen { get; set; }
-
-        /// <summary>
-        /// Gets or sets the minor tick values.
-        /// </summary>
         protected IList<double> MinorTickValues
         {
             get
@@ -154,16 +77,7 @@ namespace OxyPlot.Axes
             }
         }
 
-        /// <summary>
-        /// Gets or sets the zero grid line pen.
-        /// </summary>
         protected OxyPen ZeroPen { get; set; }
-
-        /// <summary>
-        /// Renders the specified axis.
-        /// </summary>
-        /// <param name="axis">The axis.</param>
-        /// <param name="pass">The pass.</param>
         public virtual void Render(Axis axis, int pass)
         {
             if (axis == null)
@@ -175,10 +89,6 @@ namespace OxyPlot.Axes
             this.CreatePens(axis);
         }
 
-        /// <summary>
-        /// Creates the pens.
-        /// </summary>
-        /// <param name="axis">The axis.</param>
         protected virtual void CreatePens(Axis axis)
         {
             var minorTickColor = axis.MinorTicklineColor.IsAutomatic() ? axis.TicklineColor : axis.MinorTicklineColor;
@@ -192,15 +102,6 @@ namespace OxyPlot.Axes
             this.AxislinePen = OxyPen.Create(axis.AxislineColor, axis.AxislineThickness, axis.AxislineStyle);
         }
 
-        /// <summary>
-        /// Gets the tick positions.
-        /// </summary>
-        /// <param name="axis">The axis.</param>
-        /// <param name="tickStyle">The tick style.</param>
-        /// <param name="tickSize">The tick size.</param>
-        /// <param name="position">The position.</param>
-        /// <param name="x0">The x 0.</param>
-        /// <param name="x1">The x 1.</param>
         protected virtual void GetTickPositions(Axis axis, TickStyle tickStyle, double tickSize, AxisPosition position, out double x0, out double x1)
         {
             x0 = 0;
@@ -222,13 +123,6 @@ namespace OxyPlot.Axes
             }
         }
 
-        /// <summary>
-        /// Determines whether the specified value is within the specified range.
-        /// </summary>
-        /// <param name="d">The value to check.</param>
-        /// <param name="min">The minimum value of the range.</param>
-        /// <param name="max">The maximum value of the range.</param>
-        /// <returns><c>true</c> if the specified value is within the range; otherwise, <c>false</c>.</returns>
         protected bool IsWithin(double d, double min, double max)
         {
             if (d < min)
