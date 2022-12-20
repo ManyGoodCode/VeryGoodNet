fb3bc0a47f0af117644d6d975d55072aed56cc87
ManyGoodCode <50822221+ManyGoodCode@users.noreply.github.com>
2022-12-20 23:56:21 +0800

    11

--- Good frame/oxyplot-develop (1)/Local/OxyPlot/Axes/MagnitudeAxis.cs
+++ Good frame/oxyplot-develop (1)/Local/OxyPlot/Axes/MagnitudeAxis.cs
@@ -1,24 +1,10 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="MagnitudeAxis.cs" company="OxyPlot">
-//   Copyright (c) 2014 OxyPlot contributors
-// </copyright>
-// <summary>
-//   Represents a magnitude axis for polar plots.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
 
 namespace OxyPlot.Axes
 {
     using System;
 
-    /// <summary>
-    /// Represents a magnitude axis for polar plots.
-    /// </summary>
     public class MagnitudeAxis : LinearAxis
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MagnitudeAxis" /> class.
-        /// </summary>
         public MagnitudeAxis()
         {
             this.Position = AxisPosition.None;
@@ -29,18 +15,7 @@ namespace OxyPlot.Axes
             this.MinorGridlineStyle = LineStyle.Solid;
         }
 
-        /// <summary>
-        /// Gets or sets the midpoint (screen coordinates) of the plot area. This is used by polar coordinate systems.
-        /// </summary>
         internal ScreenPoint MidPoint { get; set; }
-
-        /// <summary>
-        /// Inverse transform the specified screen point.
-        /// </summary>
-        /// <param name="x">The x coordinate.</param>
-        /// <param name="y">The y coordinate.</param>
-        /// <param name="yaxis">The y-axis.</param>
-        /// <returns>The data point.</returns>
         public override DataPoint InverseTransform(double x, double y, Axis yaxis)
         {
             var angleAxis = yaxis as AngleAxis;
@@ -59,33 +34,18 @@ namespace OxyPlot.Axes
             return new DataPoint(x, y);
         }
 
-        /// <summary>
-        /// Determines whether the axis is used for X/Y values.
-        /// </summary>
-        /// <returns><c>true</c> if it is an XY axis; otherwise, <c>false</c> .</returns>
+
         public override bool IsXyAxis()
         {
             return false;
         }
 
-        /// <summary>
-        /// Renders the axis on the specified render context.
-        /// </summary>
-        /// <param name="rc">The render context.</param>
-        /// <param name="pass">The rendering pass.</param>
         public override void Render(IRenderContext rc, int pass)
         {
             var r = new MagnitudeAxisRenderer(rc, this.PlotModel);
             r.Render(this, pass);
         }
 
-        /// <summary>
-        /// Transforms the specified point to screen coordinates.
-        /// </summary>
-        /// <param name="x">The x value (for the current axis).</param>
-        /// <param name="y">The y value.</param>
-        /// <param name="yaxis">The y axis.</param>
-        /// <returns>The transformed point.</returns>
         public override ScreenPoint Transform(double x, double y, Axis yaxis)
         {
             var angleAxis = yaxis as AngleAxis;
@@ -100,10 +60,6 @@ namespace OxyPlot.Axes
             return new ScreenPoint(this.MidPoint.x + (r * Math.Cos(theta / 180 * Math.PI)), this.MidPoint.y - (r * Math.Sin(theta / 180 * Math.PI)));
         }
 
-        /// <summary>
-        /// Updates the scale and offset properties of the transform from the specified boundary rectangle.
-        /// </summary>
-        /// <param name="bounds">The bounds.</param>
         internal override void UpdateTransform(OxyRect bounds)
         {
             double x0 = bounds.Left;
