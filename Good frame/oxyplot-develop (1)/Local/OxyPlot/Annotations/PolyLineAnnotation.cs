f24b9e489a0d87da666d39dbe7b1c95ee83d296b
ManyGoodCode <50822221+ManyGoodCode@users.noreply.github.com>
2022-12-20 22:46:28 +0800

    删除注释

--- Good frame/oxyplot-develop (1)/Local/OxyPlot/Annotations/PolyLineAnnotation.cs
+++ Good frame/oxyplot-develop (1)/Local/OxyPlot/Annotations/PolyLineAnnotation.cs
@@ -1,31 +1,13 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="PolyLineAnnotation.cs" company="OxyPlot">
-//   Copyright (c) 2014 OxyPlot contributors
-// </copyright>
-// <summary>
-//   Represents an annotation that shows a polyline.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
+
 
 namespace OxyPlot.Annotations
 {
     using System.Collections.Generic;
     using System.Linq;
 
-    /// <summary>
-    /// Represents an annotation that shows a polyline.
-    /// </summary>
     public class PolylineAnnotation : PathAnnotation
     {
-        /// <summary>
-        /// The points.
-        /// </summary>
         private readonly List<DataPoint> points = new List<DataPoint>();
-
-        /// <summary>
-        /// Gets the points.
-        /// </summary>
-        /// <value>The points.</value>
         public List<DataPoint> Points
         {
             get
@@ -34,16 +16,7 @@ namespace OxyPlot.Annotations
             }
         }
 
-        /// <summary>
-        /// Gets or sets the interpolation algorithm.
-        /// </summary>
-        /// <value>An interpolation algorithm.</value>
         public IInterpolationAlgorithm InterpolationAlgorithm { get; set; }
-
-        /// <summary>
-        /// Gets the screen points.
-        /// </summary>
-        /// <returns>The list of points to display on screen for this path.</returns>
         protected override IList<ScreenPoint> GetScreenPoints()
         {
             var screenPoints = this.Points.Select(this.Transform).ToList();
