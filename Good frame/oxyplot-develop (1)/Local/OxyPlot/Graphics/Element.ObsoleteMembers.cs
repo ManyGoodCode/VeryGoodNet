7f71d7659205fa7592b4177971847df40f38b026
ManyGoodCode <50822221+ManyGoodCode@users.noreply.github.com>
2022-12-21 00:15:39 +0800

    11

--- Good frame/oxyplot-develop (1)/Local/OxyPlot/Graphics/Element.ObsoleteMembers.cs
+++ Good frame/oxyplot-develop (1)/Local/OxyPlot/Graphics/Element.ObsoleteMembers.cs
@@ -1,67 +1,31 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="UIElement.cs" company="OxyPlot">
-//   Copyright (c) 2014 OxyPlot contributors
-// </copyright>
-// <summary>
-//   Provides an abstract base class for elements that handle mouse events.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
 
 namespace OxyPlot
 {
     using System;
 
-    /// <summary>
-    /// Provides an abstract base class for graphics elements.
-    /// </summary>
     public abstract partial class Element
     {
-        /// <summary>
-        /// Occurs when a key is pressed down when the plot view is in focus.
-        /// </summary>
         [Obsolete("Will be removed in v4.0 (#111)")]
         public event EventHandler<OxyKeyEventArgs> KeyDown;
 
-        /// <summary>
-        /// Occurs when a mouse button is pressed down on the model.
-        /// </summary>
         [Obsolete("Will be removed in v4.0 (#111)")]
         public event EventHandler<OxyMouseDownEventArgs> MouseDown;
 
-        /// <summary>
-        /// Occurs when the mouse is moved on the plot element (only occurs after MouseDown).
-        /// </summary>
         [Obsolete("Will be removed in v4.0 (#111)")]
         public event EventHandler<OxyMouseEventArgs> MouseMove;
 
-        /// <summary>
-        /// Occurs when the mouse button is released on the plot element.
-        /// </summary>
         [Obsolete("Will be removed in v4.0 (#111)")]
         public event EventHandler<OxyMouseEventArgs> MouseUp;
 
-        /// <summary>
-        /// Occurs when a touch gesture starts.
-        /// </summary>
         [Obsolete("Will be removed in v4.0 (#111)")]
         public event EventHandler<OxyTouchEventArgs> TouchStarted;
 
-        /// <summary>
-        /// Occurs when a touch gesture is changed.
-        /// </summary>
         [Obsolete("Will be removed in v4.0 (#111)")]
         public event EventHandler<OxyTouchEventArgs> TouchDelta;
 
-        /// <summary>
-        /// Occurs when the touch gesture is completed.
-        /// </summary>
         [Obsolete("Will be removed in v4.0 (#111)")]
         public event EventHandler<OxyTouchEventArgs> TouchCompleted;
 
-        /// <summary>
-        /// Raises the <see cref="MouseDown" /> event.
-        /// </summary>
-        /// <param name="e">The <see cref="OxyMouseEventArgs" /> instance containing the event data.</param>
         [Obsolete("Will be removed in v4.0 (#111)")]
         protected internal virtual void OnMouseDown(OxyMouseDownEventArgs e)
         {
@@ -71,11 +35,7 @@ namespace OxyPlot
                 handler(this, e);
             }
         }
-
-        /// <summary>
-        /// Raises the <see cref="MouseMove" /> event.
-        /// </summary>
-        /// <param name="e">The <see cref="OxyMouseEventArgs" /> instance containing the event data.</param>
+        
         [Obsolete("Will be removed in v4.0 (#111)")]
         protected internal virtual void OnMouseMove(OxyMouseEventArgs e)
         {
@@ -86,10 +46,6 @@ namespace OxyPlot
             }
         }
 
-        /// <summary>
-        /// Raises the <see cref="KeyDown" /> event.
-        /// </summary>
-        /// <param name="e">The <see cref="OxyKeyEventArgs" /> instance containing the event data.</param>
         [Obsolete("Will be removed in v4.0 (#111)")]
         protected internal virtual void OnKeyDown(OxyKeyEventArgs e)
         {
@@ -100,10 +56,6 @@ namespace OxyPlot
             }
         }
 
-        /// <summary>
-        /// Raises the <see cref="MouseUp" /> event.
-        /// </summary>
-        /// <param name="e">The <see cref="OxyMouseEventArgs" /> instance containing the event data.</param>
         [Obsolete("Will be removed in v4.0 (#111)")]
         protected internal virtual void OnMouseUp(OxyMouseEventArgs e)
         {
@@ -114,10 +66,6 @@ namespace OxyPlot
             }
         }
 
-        /// <summary>
-        /// Raises the <see cref="TouchStarted" /> event.
-        /// </summary>
-        /// <param name="e">The <see cref="OxyTouchEventArgs" /> instance containing the event data.</param>
         [Obsolete("Will be removed in v4.0 (#111)")]
         protected internal virtual void OnTouchStarted(OxyTouchEventArgs e)
         {
@@ -128,10 +76,6 @@ namespace OxyPlot
             }
         }
 
-        /// <summary>
-        /// Raises the <see cref="TouchDelta" /> event.
-        /// </summary>
-        /// <param name="e">The <see cref="OxyTouchEventArgs" /> instance containing the event data.</param>
         [Obsolete("Will be removed in v4.0 (#111)")]
         protected internal virtual void OnTouchDelta(OxyTouchEventArgs e)
         {
@@ -141,11 +85,7 @@ namespace OxyPlot
                 handler(this, e);
             }
         }
-
-        /// <summary>
-        /// Raises the <see cref="MouseUp" /> event.
-        /// </summary>
-        /// <param name="e">The <see cref="OxyTouchEventArgs" /> instance containing the event data.</param>
+        
         [Obsolete("Will be removed in v4.0 (#111)")]
         protected internal virtual void OnTouchCompleted(OxyTouchEventArgs e)
         {
